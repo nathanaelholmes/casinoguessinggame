d60025aeff67519c149d8577c3d132d4c7d458ad
Nathanael Holmes <nathanael.holmes@snhu.edu>
2020-03-27 13:15:09 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/casinoguessinggame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/casinoguessinggame.dir/main.cpp.obj"
+  "casinoguessinggame.exe"
+  "casinoguessinggame.exe.manifest"
+  "casinoguessinggame.pdb"
+  "libcasinoguessinggame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/casinoguessinggame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
